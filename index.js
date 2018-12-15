57669e3f00426eb4658ad96d8a03bc6b2f33eba7
Ally Netzel <netzelally@gmail.com>
2018-12-15 22:21:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function createPost() {
-  let commenter = document.getElementById('commenterName').value;
-  let comment = document.getElementById('commentText').value;
+  let title = document.getElementById('postTitle').value;
+  let author = document.getElementById('postAuthor').value;
 }
