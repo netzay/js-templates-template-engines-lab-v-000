8762b3c1c35a84bb5e3c8c638a60dac52e4096bb
Ally Netzel <netzelally@gmail.com>
2018-12-15 22:32:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 function createPost() {
-  let title = document.getElementById('postTitle').value;
-  let author = document.getElementById('postAuthor').value;
-  
-    let postTemplate = document.getElementById('post-template').innerHTML;
-  let templateFn = _.template(postTemplate);
- 
-  let postsDiv = document.getElementById('post');
- 
-  //execute template function with JSON object for the interpolated values
-  let templateHTML = templateFn({ comment: comment, commenter: commenter });
- 
-  //append rather than replace!
-  postsDiv.innerHTML += templateHTML;
 
+  const title = document.getElementById('postTitle').value;
+  const author = document.getElementById('postAuthor').value;
+  const body = document.getElementById('postBody').value;
+
+  const pageTemplate = _.template(document.getElementById('page-template').innerHTML);
+  const postTemplate = _.template(document.getElementById('post-template').innerHTML);
+  const commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  const blogSection = postTemplate({ 'title': title, 'body': body, 'author': author });
+  const commentsSection = commentsTemplate();
+  const postElement = document.getElementById('post');
+
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML += commentsSection;
 }
