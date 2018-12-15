3f8bb2ce3bb10072ac1bccdca4728212ea90a202
Ally Netzel <netzelally@gmail.com>
2018-12-15 22:23:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ function createPost() {
   let author = document.getElementById('postAuthor').value;
   
     let postTemplate = document.getElementById('post-template').innerHTML;
+  let templateFn = _.template(postTemplate);
+ 
+  let postsDiv = document.getElementById('post');
+ 
+  //execute template function with JSON object for the interpolated values
+  let templateHTML = templateFn({ comment: comment, commenter: commenter });
+ 
+  //append rather than replace!
+  postsDiv.innerHTML += templateHTML;
 
 }
